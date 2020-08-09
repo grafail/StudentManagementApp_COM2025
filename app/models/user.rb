1a2e6f1a38c1a49e993c6c0ebeb83f4a674d642f
Rafail Giavrimis <rafail.giavrimis@gmail.com>
2020-08-09 02:33:46 +0100

    Restrict to one role per user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,13 @@
 class User < ApplicationRecord
-  rolify
+  rolify before_add: :before_add_method
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  def before_add_method(role)
+    self.roles = []
+  end
 
   scope :staff, -> { with_any_role('staff') }
   scope :students, -> { with_any_role('student') }
