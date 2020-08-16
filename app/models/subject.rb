01e84aedbdb35430d7196d9f5d7b40380278ad4f
Rafail Giavrimis <rafail.giavrimis@gmail.com>
2020-08-16 20:34:42 +0100

    Fixed validation bug

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -6,7 +6,6 @@ class Subject < ApplicationRecord
 
   validates :course_id, :user_id, :title, presence: true
   validates :course_id, :user_id, numericality: :only_integer
-  validates_uniqueness_of :course_id, scope: [:user_id]
 
   scope :with_lecturer, ->(lecturer) { where(user: lecturer) }
   scope :student_can_enroll, ->(student) { where(course: student.course_id) }
