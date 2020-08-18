81f2ece2c07d132001d99e3017b661b25453a214
Rafail Giavrimis <rafail.giavrimis@gmail.com>
2020-08-18 23:05:56 +0100

    Added mailer test and previews

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -13,4 +13,11 @@ class HomeControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
+  test "should return contact email" do
+    mail = ContactMailer.contact_email("test@example.org",
+                                       "Matthew", "Casey", @message = "Hello")
+    assert_equal ['info@studentManagementApp.com'], mail.to
+    assert_equal ['info@studentManagementApp.com'], mail.from
+  end
+
 end
--- test/mailers/previews/contact_mailer_preview.rb
+++ test/mailers/previews/contact_mailer_preview.rb
@@ -1,4 +1,8 @@
 # Preview all emails at http://localhost:3000/rails/mailers/contact_mailer
 class ContactMailerPreview < ActionMailer::Preview
 
+  def contact_email
+    ContactMailer.contact_email("test@example.org",
+                                "Matthew", "Casey", @message = "Hello")
+  end
 end
