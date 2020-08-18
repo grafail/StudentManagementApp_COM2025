ada59dc942805e28eca0f8b49de6a7742e2f4dec
Rafail Giavrimis <rafail.giavrimis@gmail.com>
2020-08-18 22:59:08 +0100

    Changed small access detail

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -12,7 +12,8 @@ class AdminController < ApplicationController
 
   def user
     @user = User.find_by_id(params[:id])
-    if (current_user.has_role? :student and current_user.id == @user.id) or current_user.has_role? :admin or current_user.has_role? :staff
+    if @user.has_role? :staff or(current_user.has_role? :student and current_user.id == @user.id) or
+        current_user.has_role? :admin or current_user.has_role? :staff
       respond_to do |format|
         format.html { render :user }
         format.json { render json: @user, content_type: 'application/json' }
